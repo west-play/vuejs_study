06f937d12f62a0c32efc8c689975c79f1604e87b
west-play <81891300+west-play@users.noreply.github.com>
2023-05-26 17:26:53 +0900

    v05.26_17:26

---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<!-- <template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template> -->
+
+
+<template></template>
+<script>
+export default {
+  name: '',
+  components: {}, 
+  data() {
+    return {
+      sampleData: ''
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
